19867f0a4977f87985ebb8d0aa6e4b300180f7ee
ryokanni <ahmadsatriyo27@gmail.com>
2026-05-27 20:19:15 +0700

    daily commit

--- testTubesOrigin/src/main/main.java
+++ testTubesOrigin/src/main/main.java
@@ -2,12 +2,14 @@ package main;
 
 import script.background;
 import script.detektif;
+import script.character;
 
 public class Main {
     
     
     public static background bgTangga = new background("/assets/background/tangga.jpg", "Latar Tangga");
     public static detektif guwe = new detektif("/assets/character/detektif.jpg", "detektif", "Sensei");
+    public static character mutsukiHappy = new character("/assets/character/mustuki_happy.png", "mutsuki");
     
     public static void main(String[] args) {
         
